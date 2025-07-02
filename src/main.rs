68a4abf1ab720ecb52d4bc675b6ba62ed3dde6d9
AdityaP700 <126982848+AdityaP700@users.noreply.github.com>
2025-07-02 22:27:39 +0530

    Added the DELETE request

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod routes;
 use actix_web::{web, App, HttpServer};
 use std::sync::Mutex;
-use crate::routes::create_bookmark;
+use crate::routes::{delete_bookmark, get_bookmark_by_id, get_bookmarks, create_bookmark};
 use std::collections::HashMap;
 use uuid::Uuid;
 use crate::models::bookmark::Bookmark;
@@ -15,6 +15,9 @@ pub mod models;
         App::new()
         .app_data(store.clone())
         .service(create_bookmark)
+        .service(get_bookmarks)
+            .service(get_bookmark_by_id)
+            .service(delete_bookmark)
 
     })
     .bind("127.0.0.1:8080").unwrap()
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,63 +1,82 @@
-use actix_web::{web,post,Responder,HttpResponse,get};
+use crate::models::bookmark::{Bookmark, NewBookmark};
+use actix_web::{HttpResponse, Responder, delete, get, post, web};
 use chrono::Utc;
-use uuid::Uuid;
 use std::collections::HashMap;
 use std::sync::Mutex;
-use crate::models::bookmark::{Bookmark,NewBookmark};
+use uuid::Uuid;
 
 #[post("/bookmarks")]
 async fn create_bookmark(
-bookmark_data: web::Json<NewBookmark>,
-store: web::Data<Mutex<HashMap<Uuid,Bookmark>>>,)->impl Responder{
-//creating a new Bookmark instance from the input NewBookmark
-let new_data = bookmark_data.into_inner();
-   
-//Generate UUID and timestamp
-let new_bookmark = Bookmark{
-id: Uuid::new_v4(),
-    url:new_data.url,
-    title: new_data.title,
-    description:new_data.description,
-    created_at:Utc::now(), 
-};
-//Removed unwrap as we aren't Option anyone
-   println!("Received new bookmark: URL:{},Title:{:?},Description{:?},ID: {}, Created At: {}",
-   new_bookmark.url,
-   new_bookmark.title,
-   new_bookmark.description,
-   new_bookmark.id,
-   new_bookmark.created_at,
-);
-   store.lock().unwrap().insert(new_bookmark.id, new_bookmark.clone());
-   HttpResponse::Ok().body(format!("Bookmark '{}' added successfully!",
-   //toh since the title is an option so we made it unwrap 
-   new_bookmark.title.unwrap_or("Untitled".to_string())))
+    bookmark_data: web::Json<NewBookmark>,
+    store: web::Data<Mutex<HashMap<Uuid, Bookmark>>>,
+) -> impl Responder {
+    //creating a new Bookmark instance from the input NewBookmark
+    let new_data = bookmark_data.into_inner();
+
+    //Generate UUID and timestamp
+    let new_bookmark = Bookmark {
+        id: Uuid::new_v4(),
+        url: new_data.url,
+        title: new_data.title,
+        description: new_data.description,
+        created_at: Utc::now(),
+    };
+    //Removed unwrap as we aren't Option anyone
+    println!(
+        "Received new bookmark: URL:{},Title:{:?},Description{:?},ID: {}, Created At: {}",
+        new_bookmark.url,
+        new_bookmark.title,
+        new_bookmark.description,
+        new_bookmark.id,
+        new_bookmark.created_at,
+    );
+    store
+        .lock()
+        .unwrap()
+        .insert(new_bookmark.id, new_bookmark.clone());
+    HttpResponse::Ok().body(format!(
+        "Bookmark '{}' added successfully!",
+        //toh since the title is an option so we made it unwrap
+        new_bookmark.title.unwrap_or("Untitled".to_string())
+    ))
 }
 
 #[get("/bookmarks")]
-async fn get_bookmarks(
-   store: web::Data<Mutex<HashMap<Uuid, Bookmark>>>
+async fn get_bookmarks(store: web::Data<Mutex<HashMap<Uuid, Bookmark>>>) -> impl Responder {
+    let bookmarks = store.lock().unwrap();
+    let bookmarks_list: Vec<Bookmark> = bookmarks.values().cloned().collect();
 
-)->impl Responder{
-      let bookmarks = store.lock().unwrap();
-      let bookmarks_list: Vec<Bookmark> = bookmarks.values().cloned().collect();
-      
-      if bookmarks_list.is_empty() {
-         HttpResponse::NotFound().body("No bookmarks found.")
-      } else {
-         HttpResponse::Ok().json(bookmarks_list)
-  }
+    if bookmarks_list.is_empty() {
+        HttpResponse::NotFound().body("No bookmarks found.")
+    } else {
+        HttpResponse::Ok().json(bookmarks_list)
+    }
 }
+
 #[get("/bookmarks/{id}")]
 async fn get_bookmark_by_id(
-   id: web::Path<Uuid>,
-   store: web::Data<Mutex<HashMap<Uuid, Bookmark>>>
+    id: web::Path<Uuid>,
+    store: web::Data<Mutex<HashMap<Uuid, Bookmark>>>,
 ) -> impl Responder {
-   let id = id.into_inner();
-   let bookmarks = store.lock().unwrap();
+    let id = id.into_inner();
+    let bookmarks = store.lock().unwrap();
 
-   match bookmarks.get(&id) {
-      Some(bookmark) => HttpResponse::Ok().json(bookmark.clone()),
-      None => HttpResponse::NotFound().body("Bookmark not found."),
-   }
+    match bookmarks.get(&id) {
+        Some(bookmark) => HttpResponse::Ok().json(bookmark.clone()),
+        None => HttpResponse::NotFound().body("Bookmark not found."),
+    }
+}
+
+#[delete("/bookmarks/{id}")]
+async fn delete_bookmark(
+    id: web::Path<Uuid>,
+    store: web::Data<Mutex<HashMap<Uuid, Bookmark>>>,
+) -> impl Responder {
+    let id = id.into_inner();
+    let mut bookmarks = store.lock().unwrap();
+    if bookmarks.remove(&id).is_some() {
+        HttpResponse::Ok().body(format!("Bookmark with ID {} deleted successfully", id))
+    } else {
+        HttpResponse::NotFound().body(format!("Bookmark with ID {} not found", id))
+    }
 }
